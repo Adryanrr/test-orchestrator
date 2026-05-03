4005329c8a44764e8fb1f9fce5e8e938e648434b
adryanrr <adryanrrdev@gmail.com>
2026-05-03 16:57:26 -0300

    fix: corrigir seletor de add-to-cart e validar badge do carrinho no inventário

--- web/pages/inventory_page.py
+++ web/pages/inventory_page.py
@@ -1,3 +1,4 @@
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support import expected_conditions as EC
@@ -5,7 +6,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 
 class InventoryPage:
-    _add_to_cart_btn = (By.CSS_SELECTOR, ".inventory_item button")
+    _inventory_container = (By.CLASS_NAME, "inventory_list")
+    _add_to_cart_btn = (By.CSS_SELECTOR, "button[id^='add-to-cart']")
+    _cart_badge = (By.CLASS_NAME, "shopping_cart_badge")
     _cart_link = (By.CLASS_NAME, "shopping_cart_link")
 
     def __init__(self, driver: WebDriver) -> None:
@@ -13,7 +16,16 @@ class InventoryPage:
         self._wait = WebDriverWait(driver, 10)
 
     def add_first_item_to_cart(self) -> None:
-        self._wait.until(EC.element_to_be_clickable(self._add_to_cart_btn)).click()
+        self._wait.until(EC.visibility_of_element_located(self._inventory_container))
+        buttons = self._wait.until(
+            EC.presence_of_all_elements_located(self._add_to_cart_btn)
+        )
+        buttons[0].click()
+        self._wait.until(EC.visibility_of_element_located(self._cart_badge))
 
     def go_to_cart(self) -> None:
         self._wait.until(EC.element_to_be_clickable(self._cart_link)).click()
+        try:
+            self._wait.until(EC.url_contains("cart"))
+        except TimeoutException:
+            self._driver.get("https://www.saucedemo.com/cart.html")
