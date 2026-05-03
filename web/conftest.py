43d07dbd2c0f291f0de2a54a5eaa94f8c79b4462
adryanrr <adryanrrdev@gmail.com>
2026-05-03 10:45:14 -0300

    refactor: corrigir scope, remover implicitly_wait e melhorar opções do Chrome

--- web/conftest.py
+++ web/conftest.py
@@ -1,3 +1,5 @@
+from collections.abc import Generator
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -5,14 +7,14 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-@pytest.fixture(scope="session")
-def driver():
+@pytest.fixture(scope="function")
+def driver() -> Generator[webdriver.Chrome, None, None]:
     options = Options()
-    options.add_argument("--headless")
+    options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
     service = Service(ChromeDriverManager().install())
     chrome_driver = webdriver.Chrome(service=service, options=options)
-    chrome_driver.implicitly_wait(10)
     yield chrome_driver
     chrome_driver.quit()
