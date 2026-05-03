d6e9f85081cf124d86fdb5e23d66c6843f4efcbe
adryanrr <adryanrrdev@gmail.com>
2026-05-03 10:40:42 -0300

    feat: adicionar conftest e fixture do WebDriver para testes web

---
+++ web/conftest.py
@@ -0,0 +1,18 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+@pytest.fixture(scope="session")
+def driver():
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    service = Service(ChromeDriverManager().install())
+    chrome_driver = webdriver.Chrome(service=service, options=options)
+    chrome_driver.implicitly_wait(10)
+    yield chrome_driver
+    chrome_driver.quit()
